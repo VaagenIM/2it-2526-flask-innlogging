1d72587c6159e7b0e10733d653d93b0a9e95a13c
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2026-02-20 09:12:11 +0100

    feat: satt opp hosting

--- src/main.py
+++ src/main.py
@@ -2,3 +2,15 @@
 # Krypteringsfunksjon X
 
 # TODO: Lagring av brukerdata - Ser på det imorgen
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello Wørld!!!"
+
+# Dev mode:
+if __name__ == "__main__":
+    app.run(debug=True)
